f9cded262bc77ec5b6b31d9281797aaada2916dc
Vitor Machado <vitormachadobdo@gmail.com>
2023-09-30 04:29:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+    const html = document.documentElement
+  
+    //if (html.classList.contains("ligth")) {
+      //html.classList.remove("light")
+   // } else {
+     // html.classList.add("ligth")//
+   // }//
+  html.classList.toggle('ligth')
+  
+  }
